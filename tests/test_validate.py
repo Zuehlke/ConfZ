a245f33f67f04e3aeba5bcc30b7fcd8604341982
Silvan Melchior <silvan.melchior@zuehlke.com>
2021-11-11 16:24:42 +0100

    add option to validate listeners together with all configs

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -1,7 +1,7 @@
 import pytest
 from pydantic import ValidationError
 
-from confz import ConfZ, ConfZDataSource, validate_all_configs
+from confz import ConfZ, ConfZDataSource, validate_all_configs, depends_on
 
 
 def test_validate():
@@ -26,3 +26,26 @@ def test_validate():
 
     with pytest.raises(ValidationError):
         validate_all_configs()
+
+    # adjust config sources so successive test don't fail because of NewOuter2
+    NewOuter2.CONFIG_SOURCES = ConfZDataSource(data={'inner': {'attr1': 1}, 'attr2': 2})
+
+
+def test_listeners():
+    class EmptyConfig(ConfZ):
+        CONFIG_SOURCES = []
+
+    @depends_on(EmptyConfig)
+    def working_fn():
+        pass
+
+    validate_all_configs(include_listeners=False)
+    validate_all_configs(include_listeners=True)
+
+    @depends_on(EmptyConfig)
+    def broken_fn():
+        raise ValueError
+
+    validate_all_configs(include_listeners=False)
+    with pytest.raises(ValueError):
+        validate_all_configs(include_listeners=True)
